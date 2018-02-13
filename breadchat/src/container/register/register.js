54c8f59b56c875566770796b0375634d449e133c
xudan0812 <xudan0812@gmail.com>
2018-02-12 19:43:00 -0600

    Implemented user.redux.js and validate the form for registration.

--- breadchat/src/container/register/register.js
+++ breadchat/src/container/register/register.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import Logo from '../../component/logo/logo'
 import {List, InputItem, Radio, WingBlank, WhiteSpace, Button} from 'antd-mobile'
+import {register} from '../../redux/user.redux'
+import {connect} from 'react-redux'
 
+@connect (
+  state => state.user,
+  {register}
+)
 class Register extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      type:'genius'
+      username: '',
+      pwd: '',
+      confirmpwd: '',
+      type: 'genius'
     }
+    this.handleRegister = this.handleRegister.bind(this)
+  }
+
+  handleChange(key, val) {
+    this.setState({
+      [key] : val
+    })
+  }
+  handleRegister() {
+    this.props.register(this.state)
   }
 
   render(){
@@ -18,24 +37,38 @@ class Register extends React.Component{
         <h2>Register</h2>
         <WingBlank>
           <List>
-            <InputItem>Username</InputItem>
+            {this.props.msg ? <p className='error-msg'>{this.props.msg}</p>: null}
+            <InputItem
+              onChange={v=>this.handleChange('username', v)}
+            >Username</InputItem>
             <WhiteSpace/>
-            <InputItem>Password</InputItem>
+            <InputItem
+              type='password'
+              onChange={v=>this.handleChange('pwd', v)}
+            >Password</InputItem>
             <WhiteSpace/>
             <InputItem
               labelNumber={10}
+              type='password'
+              onChange={v=>this.handleChange('confirmpwd', v)}
             >Confirm Password</InputItem>
             <WhiteSpace/>
-            <RadioItem checked={this.state.type==='genius'}>
+            <RadioItem
+              checked={this.state.type==='genius'}
+              onChange={()=>this.handleChange('type', 'genius')}
+            >
               Genius
             </RadioItem>
             <WhiteSpace/>
-            <RadioItem checked={this.state.type==='boss'}>
+            <RadioItem
+              checked={this.state.type==='boss'}
+              onChange={()=>this.handleChange('type', 'boss')}
+            >
               Boss
             </RadioItem>
           </List>
           <WhiteSpace/>
-          <Button onClick={this.register} type='primary'>Register</Button>
+          <Button onClick={this.handleRegister} type='primary'>Register</Button>
         </WingBlank>
       </div>
     )
---
+++ breadchat/src/redux/user.redux.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+const ERROR_MESSAGE = 'ERROR_MESSAGE'
+const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
+
+const initState = {
+  username:'',
+  msg:'',
+  type:'',
+  pwd:'',
+  isAuth:false
+}
+
+
+export function user(state=initState, action) {
+  switch (action.type) {
+    case REGISTER_SUCCESS:
+      return {...state, isAuth:true, msg:'', ...action.payload}
+    case ERROR_MESSAGE:
+      return {...state, isAuth:false, msg: action.msg}
+    default:
+      return state
+  }
+}
+
+function errorMsg(msg) {
+  return {msg, type: ERROR_MESSAGE}
+}
+
+function registerSuccess(data) {
+  return {type: REGISTER_SUCCESS, payload: data}
+}
+
+export function register({username, pwd, confirmpwd, type}) {
+  if (!username || !pwd || !confirmpwd) {
+    return errorMsg("Please enter username and password")
+  }
+  if (pwd !== confirmpwd) {
+    return errorMsg("Password does not match")
+  }
+  return dispatch => {
+    axios.post('/user/register', {username, pwd, type})
+      .then (res =>{
+          if (res.status === 200 && res.data.code === 0) {
+            dispatch(registerSuccess({username, pwd, type}))
+          } else {
+            dispatch(errorMsg(res.data.msg))
+          }
+        }
+      )
+  }
+}
