c4d43a828932455619f71952cef7036283b5f517
xudan0812 <xudan0812@gmail.com>
2018-02-17 22:48:48 -0600

    Implemented register.

--- breadchat/server/model.js
+++ breadchat/server/model.js
@@ -1,4 +1,29 @@
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const DB_URL = 'mongodb://localhost:32769/breadchat'
+mongoose.connect(DB_URL)
 
-// const DB_URL = 'mongodb://'
-// mongoose=connect(DB_URL)
+const models = {
+  user: {
+    'username':{type: String, require: true},
+    'pwd':{type: String, require: true},
+    'type':{type: String, require: true},
+    'avatar':{type: String},
+    'desc':{type: String},
+    'title':{type: String},
+    //if boss
+    'company':{type: String},
+    'salary':{type: String}
+  },
+  chat: {
+  }
+}
+
+for (let m in models) {
+  mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+  getModel: function (name) {
+    return mongoose.model(name)
+  }
+}
--- breadchat/server/user.js
+++ breadchat/server/user.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const Router = express.Router()
+const model = require('./model')
+const User = model.getModel('user')
 
+Router.get('/list', function (req, res) {
+  User.find({}, function (err, doc) {
+    return res.json(doc)
+  })
+})
+Router.post('/register', function (req, res) {
+  const {username, pwd, type} = req.body
+  User.findOne({username}, function (err, doc) {
+    if (doc) {
+      return res.json({code: 1, msg:"username already exists"})
+    }
+    User.create({username, pwd, type}, function (e, d) {
+      if (e) {
+        return res.json({code: 1, msg:"server error"})
+      }
+      return res.json({code: 0})
+    })
+  })
+})
 Router.get('/info', function(req, res){
   return res.json({code: 1})
 })
